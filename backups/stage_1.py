233485002a0175f314a2c2d8cb40be75f6839123
ABDURAHIMAN K <laluvga@gmail.com>
2023-06-05 10:26:28 +0530

    stage 1

---
+++ backups/stage_1.py
@@ -0,0 +1,14 @@
+# Project: Web Scraper
+# Stage 1/5: Wanna Talk to the Internet?
+import requests
+
+
+url = input()
+response = requests.get(url)
+if response.status_code == 200:
+    try:
+        print("\n", response.json()['content'], sep="")
+    except KeyError:
+        print("\nInvalid quote resource!")
+else:
+    print("\nInvalid quote resource!")
